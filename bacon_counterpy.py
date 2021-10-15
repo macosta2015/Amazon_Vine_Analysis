176c4e69efa1a60124bd81be62227d779a1d885c
macosta2015 <macosta2015@my.fit.edu>
2021-10-14 21:05:54 -0400

    Uploading text file

--- bacon_counterpy.py
+++ bacon_counterpy.py
@@ -8,3 +8,19 @@ class Bacon_count(MRJob):
 def mapper(self, _, line):
 #The second parameter (here using an underscore (_), explained next) allows methods to be mapped together. 
 #Since we are not chaining anything together, we use the Python convention of an underscore to indicate that we won’t use this parameter. The line parameter will be the line of text taken from the raw input file.
+
+#Use the code to Yield functions
+from mrjob.job import MRJob
+class Bacon_count(MRJob):
+   def mapper(self, _, line):
+       for word in line.split():
+           if word.lower() == "bacon":
+               yield "bacon", 1
+
+   def reducer(self, key, values):
+       yield key, sum(values)
+if __name__ == "__main__":
+   Bacon_count.run()
+
+#The mrjob library works by reading in a file passed to it in the terminal.
+
